84875fdc68788e578ac21f4ae6af05d2c4f1419d
Ishan Sharma <ishandeveloper@outlook.com>
2020-03-29 11:08:57 +0530

    Deploying without Mongo

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -4,7 +4,7 @@ var mongoose=require('mongoose');
 
 //Connecting To MongoDB
 
-mongoose.connect('mongodb+srv://ishandeveloper:test@todoapp-wnpfa.mongodb.net/test?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true});
+// mongoose.connect('mongodb+srv://ishandeveloper:test@todoapp-wnpfa.mongodb.net/test?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true});
 // mongoose.connect('mongodb+srv://ID:Pass@abc', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).catch(error => handleError(error));
 //Create Schema
 
@@ -14,7 +14,7 @@ var todoSchema= new mongoose.Schema({
 
 //Model
 
-var Todo=mongoose.model('Todo',todoSchema);
+// var Todo=mongoose.model('Todo',todoSchema);
 
 // var testitem=Todo({item:'Go To Bed'}).save((err)=>{
 //     if(err) throw err;
@@ -22,31 +22,39 @@ var Todo=mongoose.model('Todo',todoSchema);
 // });
 
 //Dummy Data For Testing
-// var data=[{item:'Get Grocery From Market'},{item:'Take Dog For Walk'},{item:'Boil Milk'}];
+var data=[{item:'Get Grocery From Market'},{item:'Take Dog For Walk'},{item:'Boil Milk'}];
 
 module.exports=(app)=>{
 
     app.get('/',(req,res)=>{
         res.redirect("/todo");
+        
     });
     app.get('/todo', (req,res)=>{
-        Todo.find({},(err,data)=>{
-            if (err) throw err;
-        res.render('todo.ejs',{todos:data}); 
-        });
+        // Todo.find({},(err,data)=>{
+        //     if (err) throw err;
+        // res.render('todo.ejs',{todos:data}); 
+        // });
+        res.render('todo.ejs',{todos:data});
     });
 
     app.post('/todo', urlEncodedParser, (req,res)=>{
-        var newTodo=Todo(req.body).save((err,data)=>{
-            if(err) throw err;
-            res.json({todos: data});
-            });
+        // var newTodo=Todo(req.body).save((err,data)=>{
+        //     if(err) throw err;
+        //     res.json({todos: data});
+        //     });
+        data.push(req.body);
+        res.json({todos: data});
     });
 
     app.delete('/todo/:item', (req,res)=>{
-        Todo.find({item:req.params.item.replace(/\-/g," ")}).deleteOne((err,data)=>{
-            if(err) throw err;
-            res.json({todos: data});
+        // Todo.find({item:req.params.item.replace(/\-/g," ")}).deleteOne((err,data)=>{
+        //     if(err) throw err;
+        //     res.json({todos: data});
+        // });
+        data=data.filter((todo)=>{
+            return todo.item.replace(/ /g,'-')!== req.params.item;
         });
+        res.json(data);
     });
 };
