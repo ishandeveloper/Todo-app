3c78ab650702e1d6cbfa168e2b9ca6d4e1d3088c
Ishan Sharma <ishandeveloper@outlook.com>
2020-03-29 13:28:56 +0530

    Optimized

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express=require('express');
-var app=express();
-var todoController=require('./controllers/todo-controller');
+var express = require('express');
+var app = express();
+var todoController = require('./controllers/todo-controller');
 
 //Setting Template Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //Serving Static Files
-app.use('/assets',express.static('assets'));
+app.use('/assets', express.static('assets'));
 
 //Firing Controllers
 todoController(app);
@@ -14,4 +14,4 @@ todoController(app);
 //Running Up The Server
 var port = process.env.PORT || 8080;
 app.listen(port);
-console.log("Server Up at Port: "+port);
+console.log("Server Up at Port: " + port);
--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,20 +1,25 @@
-var bodyParser=require('body-parser');
-var urlEncodedParser=bodyParser.urlencoded({extended:false});
-var mongoose=require('mongoose');
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: false
+});
+var mongoose = require('mongoose');
 
 //Connecting To MongoDB
 
-mongoose.connect('mongodb+srv://ishandeveloper:test@todoapp-wnpfa.mongodb.net/test?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect('mongodb+srv://ishandeveloper:test@todoapp-wnpfa.mongodb.net/test?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 // mongoose.connect('mongodb+srv://ID:Pass@abc', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).catch(error => handleError(error));
 //Create Schema
 
-var todoSchema= new mongoose.Schema({
-    item:String
+var todoSchema = new mongoose.Schema({
+    item: String
 });
 
 //Model
 
-var Todo=mongoose.model('Todo',todoSchema);
+var Todo = mongoose.model('Todo', todoSchema);
 
 // var testitem=Todo({item:'Go To Bed'}).save((err)=>{
 //     if(err) throw err;
@@ -24,29 +29,40 @@ var Todo=mongoose.model('Todo',todoSchema);
 //Dummy Data For Testing
 // var data=[{item:'Get Grocery From Market'},{item:'Take Dog For Walk'},{item:'Boil Milk'}];
 
-module.exports=(app)=>{
+module.exports = (app) => {
 
-    app.get('/',(req,res)=>{
+    app.get('/', (req, res) => {
         res.redirect("/todo");
     });
-    app.get('/todo', (req,res)=>{
-        Todo.find({},(err,data)=>{
+
+
+    app.get('/todo', (req, res) => {
+        Todo.find({}, (err, data) => {
             if (err) throw err;
-        res.render('todo.ejs',{todos:data}); 
+            res.render('todo.ejs', {
+                todos: data
+            });
         });
     });
 
-    app.post('/todo', urlEncodedParser, (req,res)=>{
-        var newTodo=Todo(req.body).save((err,data)=>{
-            if(err) throw err;
-            res.json({todos: data});
+    app.post('/todo', urlEncodedParser, (req, res) => {
+        var newTodo = Todo(req.body).save((err, data) => {
+            if (err) throw err;
+            res.json({
+                todos: data
             });
+        });
+        // res.redirect('/todo'); 
     });
 
-    app.delete('/todo/:item', (req,res)=>{
-        Todo.find({item:req.params.item.replace(/\-/g," ")}).deleteOne((err,data)=>{
-            if(err) throw err;
-            res.json({todos: data});
+    app.delete('/todo/:item', (req, res) => {
+        Todo.find({
+            item: req.params.item.replace(/\-/g, " ")
+        }).deleteOne((err, data) => {
+            if (err) throw err;
+            res.json({
+                todos: data
+            });
         });
     });
 };
